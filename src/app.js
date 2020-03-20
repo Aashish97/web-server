daba03a00844666d85c93364cceba32aec604124
aashish <aashish@ubuntu>
2020-03-20 18:38:19 +0545

    Changed static content to dynamic using hbs

--- src/app.js
+++ src/app.js
@@ -6,7 +6,32 @@ const app = express();
 
 const publicDirectoryPath = path.join(__dirname, './../public')
 
+//sets the view engine value for a given express setting
+app.set('view engine', 'hbs');
+
+//load static contents from the given directory path
 app.use(express.static(publicDirectoryPath));
+
+app.get('', (req, res) => {
+    //renders the views whose name matches the string
+    res.render('index', {
+        title: "Weather",
+        name: 'Aashish'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: "About",
+        name: 'Aashish'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: "This is a help message"
+    })
+})
 
 app.get('/weather', (req,res) => {
     res.send({
