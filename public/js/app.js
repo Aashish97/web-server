d1fe6ee7b9647e2c8f49c7f0fd07a8b30df8fda2
aashish <aashish@ubuntu>
2020-03-21 13:47:38 +0545

    added fetch functionality to get the data from the url

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,9 @@
-console.log("Loading client side javascript")
+fetch('http://localhost:3000/weather?address=theaskjalsjd').then((response) => {
+    response.json().then((data) => {
+        if(data.error){
+            console.log(data.error);
+        }else{
+            console.log(data.location, data.summary);
+        }
+    })
+})
