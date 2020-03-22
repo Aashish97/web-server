4eda685806ec815bfc5a0881366a0fce33a850d6
aashish <aashish@ubuntu>
2020-03-22 14:08:01 +0545

    Changed the temperature into celsius

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -3,7 +3,7 @@ const request = require('request');
 //forecast takes longitude and latitude as a input and provides the weather information accordingly
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/ab50cd36126b4928f245dedbfb477f85/' + latitude + ',' + longitude;
+    const url = 'https://api.darksky.net/forecast/ab50cd36126b4928f245dedbfb477f85/' + latitude + ',' + longitude + '?units=si';
     request( {url, json: true}, (error, {body}) => {
         if (error){
             callback("Unable to connect with weather API!!!", undefined);
